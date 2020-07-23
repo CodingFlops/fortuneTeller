b9f554e48311e638fc0e9f4952388fdd87722db1
CodingFlops <flopsiew@gmail.com>
2020-07-23 15:37:27 -0700

    finished fortune teller

--- fortuneteller.py
+++ fortuneteller.py
@@ -1,2 +1,12 @@
-def fortune_teller(fortune):
-    Q = input()
+import random
+def fortune_teller():
+    Ass = "Yes"
+    while(Ass == "Yes"):
+        Q = input("Enter your future ")
+        number = random.randint(1, 2)
+        if number == 1:
+            print(Q, "?")
+        elif number == 2:
+            print(Q, "!")
+        Ass = input("Do you want continue?  Yes or No? ") 
+fortune_teller()
